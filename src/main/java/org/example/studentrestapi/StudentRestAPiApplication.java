87a6fa649391484bc5cf51f5f1331de7a27af08d
chayan <chayanchowdhury@gmail.com>
2024-05-05 22:49:32 +0530

    Initial commit

---
+++ src/main/java/org/example/studentrestapi/StudentRestAPiApplication.java
@@ -0,0 +1,13 @@
+package org.example.studentrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRestAPiApplication.class, args);
+    }
+
+}
