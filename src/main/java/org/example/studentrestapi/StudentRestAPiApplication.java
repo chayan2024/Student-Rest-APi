bce831eac7d1131b38db83ff4daecbcfd8ff5050
chayan <chayanchowdhury@gmail.com>
2024-05-17 16:58:43 +0530

    handled Error

--- src/main/java/org/example/studentrestapi/StudentRestAPiApplication.java
+++ src/main/java/org/example/studentrestapi/StudentRestAPiApplication.java
@@ -1,10 +1,13 @@
 package org.example.studentrestapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StudentRestAPiApplication {
 
     public static void main(String[] args) {
